541b40c44b9cb74a080353ce46b1d1568504828e
osher-steel <111786194+osher-steel@users.noreply.github.com>
2023-01-14 13:58:32 -0500

    Update WebToPigLatin.java

--- WebToPigLatin.java
+++ WebToPigLatin.java
@@ -6,79 +6,15 @@ import java.io.IOException;
 
 public class WebToPigLatin {
 
-	public static boolean isContent(char c) {
-		if (c == '<' || c == '&')
-			return false;
-		else
-			return true;
-	}
-
-	public static boolean isAlphaNum(char c) {
-		if ((Character.toLowerCase(c) >= 97 && Character.toLowerCase(c) <= 122) || c == 39)
-			return true;
-		else
-			return false;
-	}
-
-	public static boolean isVowel(char c) {
-		c = Character.toLowerCase(c);
-
-		if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y')
-			return true;
-		else
-			return false;
+	public static void main(String[] args) throws IOException {
+		if (args.length != 0) {
+			System.out.println("Usage: java WebToPigLatin inputFile outputFile");
+			System.out.println("Must have two command-line parameters");
+		} else
+			fileRead("new", args[1]);
 
 	}
-
-	public static boolean isVowelWithoutY(char c) {
-		c = Character.toLowerCase(c);
-
-		if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
-			return true;
-		else
-			return false;
-	}
-
-	public static int writeWord(BufferedReader br, BufferedWriter bw, int intRead, char firstLetter, boolean upperCase)
-			throws IOException {
-		BufferedReader tempBr = new BufferedReader(new FileReader("temp2"));
-		int first = 0;
-		int tempRead;
-		while (isAlphaNum((char) intRead)) {
-			if (first != 0)
-				intRead = br.read();
-
-			if (isAlphaNum((char) intRead)) {
-				if (first == 0 && upperCase == true) // Sends first letter as an uppercase
-					bw.write(Character.toUpperCase((char) intRead));
-				else
-					bw.write((char) intRead);
-			} else // If end of word
-			{
-				if (isVowelWithoutY(firstLetter)) // If word started with a vowel (not y) then add a 'w'
-					bw.write('w');
-				else {
-					while ((tempRead = tempBr.read()) != ' ')// If word started with a consonant
-					{
-						bw.write(Character.toLowerCase((char) tempRead)); // print all the consonants from the temp file
-					}
-
-				}
-
-				bw.write("ay"); // add "ay" at end of word
-				tempBr.close();
-
-				return intRead; // returns intRead to make sure that the last letter is analyzed
-			}
-
-			first++; // To make sure that only the first letter can be capitalized
-
-		}
-
-		tempBr.close();
-		return 0;
-	}
-
+	
 	public static void fileRead(String in, String out) throws IOException {
 		BufferedReader br = new BufferedReader(new FileReader(in));
 		BufferedWriter bw = new BufferedWriter(new FileWriter(out));
@@ -113,7 +49,7 @@ public class WebToPigLatin {
 							intRead = br.read();
 						}
 						while ((isVowel((char) intRead) == false)) // Loop that gets all the successive consonants at										// the
-						{ 											//the beginning of the word
+						{ 					   //the beginning of the word
 							tempBw.write(intRead); // Prints them in a temporary file
 							intRead = br.read();
 						}
@@ -157,14 +93,66 @@ public class WebToPigLatin {
 		bw.close();
 
 	}
+	
+	public static int writeWord(BufferedReader br, BufferedWriter bw, int intRead, char firstLetter, boolean upperCase)
+			throws IOException {
 
-	public static void main(String[] args) throws IOException {
-		if (args.length != 0) {
-			System.out.println("Usage: java WebToPigLatin inputFile outputFile");
-			System.out.println("Must have two command-line parameters");
-		} else
-			fileRead("new", args[1]);
+		BufferedReader tempBr = new BufferedReader(new FileReader("temp2"));
+		int first = 0;
+		int tempRead;
 
+		while (isAlphaNum((char) intRead)) {
+			if (first != 0)
+				intRead = br.read();
+
+			if (isAlphaNum((char) intRead)) {
+				if (first == 0 && upperCase == true) // Sends first letter as an uppercase
+					bw.write(Character.toUpperCase((char) intRead));
+				else
+					bw.write((char) intRead);
+			} else // If end of word
+			{
+				if (isVowelWithoutY(firstLetter)) // If word started with a vowel (not y) then add a 'w'
+					bw.write('w');
+				else {
+					while ((tempRead = tempBr.read()) != ' ')// If word started with a consonant
+					{
+						bw.write(Character.toLowerCase((char) tempRead)); // print all the consonants from the temp file
+					}
+
+				}
+
+				bw.write("ay"); // add "ay" at end of word
+				tempBr.close();
+
+				return intRead; // returns intRead to make sure that the last letter is analyzed
+			}
+
+			first++; // To make sure that only the first letter can be capitalized
+
+		}
+
+		tempBr.close();
+		return 0;
+	}
+	
+	public static boolean isContent(char c) {
+		return (c == '<' || c == '&');
 	}
 
+	public static boolean isAlphaNum(char c) {
+		return((Character.toLowerCase(c) >= 97 && Character.toLowerCase(c) <= 122) || c == 39);
+	}
+
+	public static boolean isVowel(char c) {
+		c = Character.toLowerCase(c);
+
+		return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y');
+	}
+
+	public static boolean isVowelWithoutY(char c) {
+		c = Character.toLowerCase(c);
+
+		return(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+	}
 }
